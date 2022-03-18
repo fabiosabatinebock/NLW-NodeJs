713c906149d21bf8292fb2e159614a663cce7daf
fabiosabatinebock <fabiosabatinejuina@gmail.com>
2022-03-18 09:11:00 -0400

    Mudança no server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,7 @@
 import "reflect-metadata";
-<<<<<<< HEAD
 import express, { Request, Response, NextFunction} from "express";
-=======
-import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 
->>>>>>> 9dc105ec521626a45b565dbcc44f46cf093a2ec6
 import { router } from "./routes";
 import "express-async-errors";
 
@@ -17,7 +13,6 @@ app.use(express.json());
 
 app.use(router);
 
-<<<<<<< HEAD
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
   if(err instanceof Error){
     return response.status(400).json({
@@ -33,21 +28,7 @@ app.use((err: Error, request: Request, response: Response, next: NextFunction) =
 
 
 app.listen(3000, () => console.log("Server is running"));
-=======
-app.use((err: Error, request: Request, response: Response, next: NextFunction ) => {
-    if (err instanceof Error){
-      return response.status(400).json({
-          error : err.message
-      })    
-    }
 
-    return response.status(500).json({
-        status : "error",
-        message : "Internal Server Error"
-    })
-})
-
->>>>>>> 9dc105ec521626a45b565dbcc44f46cf093a2ec6
 /**
  * Get => Buscar informações
  * Post => Inserir (Criar) informações
